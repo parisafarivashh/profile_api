f32d2028d2d14800ef634477bc1582194469db9a
parisafarivashh <parisafarivash@gmail.com>
2020-02-19 08:50:02 +0200

    add model

--- profile_api/restapi/models.py
+++ profile_api/restapi/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Profile(models.Model):
+  name = models.CharField(max_length=100)
+  last_name = models.CharField(max_length=100)
+  phone = models.IntegerField()
+
+  def __str__(self):
+    return self.name
